8719bc5363d98c74cc295f24c11473591d27261d
sunny mishra <mishrasunny174@gmail.com>
2018-03-24 23:38:50 +0530

    Basic Building Block

---
+++ DictionaryApp/src/main/DictionaryApp.java
@@ -0,0 +1,9 @@
+package main;
+
+public class DictionaryApp {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ DictionaryApp/src/testunits/dictionaryConnectionTest.java
@@ -0,0 +1,11 @@
+package testunits;
+
+import dictionary.dictionaryConnection;
+
+public class dictionaryConnectionTest {
+
+	public static void main(String[] args) {
+		System.out.println(new dictionaryConnection().getResponse("love"));
+	}
+
+}
