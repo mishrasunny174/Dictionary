6dc37d3e3f37e2d840dc2a6a0786b4d4c6233b2a
sunny mishra <mishrasunny174@gmail.com>
2018-03-25 15:31:15 +0530

    Done GUI

--- DictionaryApp/src/main/DictionaryApp.java
+++ DictionaryApp/src/main/DictionaryApp.java
@@ -1,9 +1,17 @@
 package main;
 
+import javax.swing.SwingUtilities;
+
+import gui.GUIFrame;
+
 public class DictionaryApp {
 
 	public static void main(String[] args) {
-		
+		SwingUtilities.invokeLater(() -> {
+			GUIFrame app = new GUIFrame("Dictionary");
+			app.init();
+			app.run();
+		});
 	}
 
 }
